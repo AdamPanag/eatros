785d3aefc2bb91bf3c887854cef2a4cd404d81d5
AdamPanag <adampanag06@gmail.com>
2020-10-02 14:44:27 +0300

    Add login & register for all users
    Also, prevent a user that has not logged in to access the search-doctors page

--- mysite/eatros/urls.py
+++ mysite/eatros/urls.py
@@ -4,7 +4,12 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+
     path('search-doctors/', views.search_doctors, name='search-doctors'),
-    path('auth/login.html', views.loginPage, name='loginPage'),
-    path('auth/register.html', views.register, name='register'),
+
+    path('auth/patient-login.html', views.patient_login, name='patient-login'),
+    path('auth/doctor-login.html', views.doctor_login, name='doctor-login'),
+
+    path('auth/patient-register.html', views.patient_register, name='patient-register'),
+    path('auth/doctor-register.html', views.doctor_register, name='doctor-register'),
 ]
--- mysite/eatros/views.py
+++ mysite/eatros/views.py
@@ -18,19 +18,55 @@ def index(request):
 
     return render(request, 'eatros/index.html', context)
 
-def search_doctors(request):
-    
-    return render(request, 'eatros/search-doctors.html', {})
 
-def loginPage(request):
+def search_doctors(request):
+    if request.user.is_authenticated:
+        context = {
+            'showLoginBtn' : True
+        }
+        return render(request, 'eatros/search-doctors.html', context)
+    else:
+        messages.error(request, 'You must login first.')
+        return redirect('/')
+
+
+def patient_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            messages.error(request, 'Wrong username or password.')
 
     context = {
         'showLoginBtn' : False
     }
 
-    return render(request, 'eatros/auth/login.html', context)
+    return render(request, 'eatros/auth/patient-login.html', context)
 
-def register(request):
+
+def doctor_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            messages.error(request, 'Wrong username or password.')
+
+    context = {
+        'showLoginBtn' : False
+    }
+
+    return render(request, 'eatros/auth/doctor-login.html', context)
+
+
+def patient_register(request):
     # if the user has already loged in
     if request.user.is_authenticated:
         return redirect('/')
@@ -42,11 +78,33 @@ def register(request):
                 user = form.save()
                 messages.success(request, 'Account was created for ' + user.username)
                 
-                return redirect('login.html')
+                return redirect('patient-login.html')
             
         context = {
             'form':form,
             'showLoginBtn' : False
         }
         # not logged in and not POST method
-        return render(request, 'eatros/auth/register.html', context) 
+        return render(request, 'eatros/auth/patient-register.html', context)
+
+
+def doctor_register(request):
+    # if the user has already loged in
+    if request.user.is_authenticated:
+        return redirect('/')
+    else:
+        form = DoctorSignUpForm()
+        if request.method == 'POST':
+            form = DoctorSignUpForm(request.POST)
+            if form.is_valid(): # successfully registered
+                user = form.save()
+                messages.success(request, 'Account was created for ' + user.username)
+                
+                return redirect('doctor-login.html')
+            
+        context = {
+            'form':form,
+            'showLoginBtn' : False
+        }
+        # not logged in and not POST method
+        return render(request, 'eatros/auth/doctor-register.html', context) 
